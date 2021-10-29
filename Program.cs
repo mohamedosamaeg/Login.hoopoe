8157af82a403be92e0f46e0e3d47c1b323929e2a
mohamedoselim <imokhamedusamaelsai@urfu.me>
2021-10-29 15:39:52 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Login.hoopoe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string UserName;
+            string Password;
+            Console.WriteLine("Hello my dear customer,we have to know your user name & password To Login to our Community :)" );
+            Console.WriteLine("User name :");
+            UserName = Console.ReadLine();
+            Console.WriteLine("Password :");
+            Password = Console.ReadLine();
+            if (UserName == "mohamed" || UserName == "ahmed" || UserName == "reham" || UserName == "mona" && Password == "hoopoe2021")
+            {
+                Console.WriteLine("WELCOME :)" + UserName);
+            }
+            else
+            {
+                Console.WriteLine("Username or password are wrong");
+
+
+            }
+            
+
+
+        }
+    }
+}
